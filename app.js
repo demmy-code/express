b111a7ee342c257af56e89c86ead744d10c23104
Demilade <dolaniyan32@gmail.com>
2026-05-12 12:55:41 +0100

    Almost done

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/user', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/user/age', (req, res) => {
+  res.send('User is 25 years old')
+})
+
+app.get('/user/age/date', (req, res) => {
+  res.send('User registered his age on 2020-01-01')
+})
+
+app.post('/user', (req, res) => {
+  res.send('This is a new user')
+})
+
+app.post('/user/name', (req, res) => {
+  res.send('The  name of the user is Demi')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
-
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user')
 })
 
