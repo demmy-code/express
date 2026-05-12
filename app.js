e18613304a704f56a1dbe9193fa05b59d63279dd
Demilade <dolaniyan32@gmail.com>
2026-05-12 16:20:30 +0100

    nearly there

--- app.js
+++ app.js
@@ -37,3 +37,20 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+
+const cb0 = function (req, res, next) {
+  console.log('CB0')
+  next()
+}
+
+const cb1 = function (req, res, next) {
+  console.log('CB1')
+  next()
+}
+
+const cb2 = function (req, res) {
+  res.send('Hello from C!')
+}
+
+app.get('/example/c', [cb0, cb1, cb2])
+
